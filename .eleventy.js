3d48d34ee11a159be79de80ba4535f4a8749f0d0
Rémi Faure <djaroul@hotmail.fr>
2023-03-30 18:51:48 +0200

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+/* Config file */
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("./src/images");
+    eleventyConfig.addPassthroughCopy("./src/css");
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+};
